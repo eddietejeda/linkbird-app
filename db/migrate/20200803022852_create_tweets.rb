ba11e76b7c41f9c7f54c52a28e294c1f7275becb
Eddie A Tejeda <eddie.tejeda@gmail.com>
2020-08-04 23:04:47 -0400

    provide link to tweet_id

--- db/migrate/20200803022852_create_tweets.rb
+++ db/migrate/20200803022852_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table  :tweets do |t|
       t.integer   :user_id,     null: false
-      t.bigint    :keys,        null: false,  default: '',  unique: true
+      t.bigint    :tweet_id,    null: false,  unique: true
       t.jsonb     :tweet,       null: false,  default: '{}'
     end
   end
