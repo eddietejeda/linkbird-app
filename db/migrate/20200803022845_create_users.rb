db0ecd4ab258087f42debde6be7a082b6907cfdf
Eddie A Tejeda <eddie.tejeda@gmail.com>
2020-08-05 22:09:10 -0400

    make sure uid is bigint

--- db/migrate/20200803022845_create_users.rb
+++ db/migrate/20200803022845_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table  :users do |t|
-      t.integer   :uid,         null: false
+      t.bigint    :uid,         null: false
       t.string    :cookie_key,  null: false
       t.jsonb     :json,        null: false,  default: '{}'
       t.timestamps
