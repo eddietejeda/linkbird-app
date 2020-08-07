c80f5652a3733ee3491bd2d05b7b807ec5f478e8
Eddie A Tejeda <eddie.tejeda@gmail.com>
2020-08-07 14:52:13 -0400

    refresh every 20 minutes

--- app.rb
+++ app.rb
@@ -28,9 +28,10 @@ class App < Sinatra::Base
       user = User.where(" uid = :uid ", { uid: session[:uid] } ).first_or_create( uid: session[:uid], cookie_key: cookies["key"] ) 
       @first_download = (user && user.tweets.length == 0)
       
-      last_tweet_created_at = (Tweet.order("created_at").last && Tweet.order("created_at").last.created_at.getlocal("+00:00")) || 20.minutes.ago.getlocal("+00:00")
+      last_tweet_created_at = (Tweet.order("created_at").last && Tweet.order("created_at").last.created_at.getlocal("+00:00")) || 30.minutes.ago.getlocal("+00:00")
+      last_update_in_minutes = ((Time.now.getlocal("+00:00").to_i - last_tweet_created_at.to_i)) / 60
       
-      if user && (last_tweet_created_at < 30.minutes.ago.getlocal("+00:00"))
+      if user && last_update_in_minutes > 20
         if settings.development?
           DownloadTweetWorker.new.perform( user.id, session[:access_token], session[:access_token_secret] )
         else settings.production?
