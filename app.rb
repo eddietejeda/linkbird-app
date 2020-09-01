84380b34b091825b492c52a1f12d2a5a5aab3bec
Eddie A Tejeda <eddie.tejeda@gmail.com>
2020-09-01 09:49:02 -0400

    limit to last 100 entries

--- app.rb
+++ app.rb
@@ -42,7 +42,7 @@ class App < Sinatra::Base
       end
       # @page = params['page'].to_i || 0
       # @length = user.tweets.length
-      @tweets = user.tweets.order(tweet_date: :asc) #.limit(10).order(created_at: :desc).limit(10).offset(@page * 10)
+      @tweets = user.tweets.order(tweet_date: :asc).limit(100) #.limit(10).order(created_at: :desc).limit(10).offset(@page * 10)
     end
 
     erb :index
