385e9012bea346eadd02015aa1a0b8c5d2c2c03b
Eddie A Tejeda <eddie.tejeda@gmail.com>
2020-09-27 09:49:56 -0400

    default value should be array

--- db/migrate/20200926203351_add_security_keys_to_users.rb
+++ db/migrate/20200926203351_add_security_keys_to_users.rb
@@ -1,7 +1,7 @@
 class AddSecurityKeysToUsers < ActiveRecord::Migration[6.0]
   def change
     rename_column :users, :cookie_key, :secret_key
-    add_column :users, :cookie_keys, :jsonb,  default: {}
+    add_column :users, :cookie_keys, :jsonb,  default: []
   end
 end
 
