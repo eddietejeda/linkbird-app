886148c329202febbd57bc8b8fc4c466666cb429
Eddie A Tejeda <eddie.tejeda@gmail.com>
2020-10-01 13:49:08 -0400

    add logger instead is puts and decrease max tweets per user

--- lib/workers/delete_old_tweets_worker.rb
+++ lib/workers/delete_old_tweets_worker.rb
@@ -3,18 +3,16 @@ class DeleteOldTweetsWorker
   sidekiq_options retry: 0
 	
   def perform(user_id)
-
-    max_tweets = 1000
+    
+    max_tweets = 250
 
     user = User.find_by( id: user_id )
-    puts "Current user_id=#{user.id} has #{user.tweets.size} tweets"
+    logger.info "Current user_id=#{user.id} has #{user.tweets.size} tweets"
+
     if user.tweets.size > max_tweets
-      
       oldest_tweet_id = user.tweets.order(id: :desc).limit(max_tweets).last.id
-      
       result = user.tweets.where("id < :id", {id: oldest_tweet_id} ).destroy_all
-      puts "DELETING #{result.count}"
-      
+      logger.info "DELETING #{result.count}"
     end
 
   end
