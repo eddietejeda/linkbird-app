cb7e327eda21f7a53a7305c637137ba60f336fd6
Eddie A Tejeda <eddie.tejeda@gmail.com>
2020-10-03 22:43:52 -0400

    add new method of bot

--- lib/helpers/twitter.rb
+++ lib/helpers/twitter.rb
@@ -7,3 +7,15 @@ def get_twitter_connection(token, secret)
   end  
 end
 
+
+
+def get_twitter_bot_connection
+  Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_API_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_API_CONSUMER_SECRET"]
+    
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"] 
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end  
+end
+
